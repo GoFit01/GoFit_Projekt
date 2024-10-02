56e8b29646a7f0be20037385ecb3ed5aedf00089
fzksbalazs <90751896+szekelyaron@users.noreply.github.com>
2024-10-02 18:13:32 +0200

    Revert "Adatbazis letrehozasa"
    This reverts commit 2bddb777d2719b18a51739710bd5092ee2f5da99.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,10 @@
-const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 var cors = require("cors");
 
-dotenv.config(); //env fileból toltes
+dotenv.config();
 
-
-mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB csatlakozás sikeresen megtörtént!"))
@@ -16,8 +13,8 @@ mongoose
   });
 
 
-  app.use(cors()); //Google miatt kell + frontend backend osszekotes
-  app.use(express.json());
+  app.use(cors());
+
 
   app.listen(process.env.PORT || 5000, () => {
     console.log("Backend szerver fut!");
